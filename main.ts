220e05224392658ab5a3cac43251086cf8ed2440
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2022-02-22 18:27:49 -0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -8,18 +8,22 @@
 namespace simplified {
 
     /**
-     * Allows your sprite to jump only if it's currently on a floor
+     * Allows your sprite to jump only if it's currently
+     * on a floor or without velocity.
+     * Expand the + to add jumping power.
      */
     //% group=Sprites
     //% color="#3b6fe9"
     //% blockId=gravity_jump
-    //% block="make $thisSprite=variables_get(mySprite) gravity jump" || with power $velo
-    export function gravity_jump(thisSprite: Sprite, velo: number) {
+    //% block="make $thisSprite=variables_get(mySprite) gravity jump || with power $velo"
+    //% velo.defl=-200
+    //% help=github:simple-blocks/docs/gravity-jump
+    export function gravity_jump(thisSprite: Sprite, velo?: number) {
         if (!velo) {
-            velo = 200;
+            velo = -200;
         }
-        if (thisSprite.isHittingTile(CollisionDirection.Bottom)) {
-            thisSprite.vy = Math.abs(velo)
+        if (thisSprite.vy == 0 || thisSprite.isHittingTile(CollisionDirection.Bottom) || (thisSprite.bottom >= (scene.cameraTop() + 120))) {
+            thisSprite.vy = -Math.abs(velo)
         }
     }
 
@@ -39,6 +43,7 @@ namespace simplified {
     //% blockId=choose_random_from_array
     //% block="choose one of $choice1=screen_image_picker $choice2=screen_image_picker || $choice3=screen_image_picker $choice4=screen_image_picker $choice5=screen_image_picker"
     //% inlineInputMode=inline
+    //% help=github:simple-blocks/docs/choose-sprite
     export function chooseRandomImage(choice1: Image, choice2: Image, choice3?: Image, choice4?: Image, choice5?: Image) {
         let myList = [choice1, choice2];
         if (choice3) myList.push(choice3);
