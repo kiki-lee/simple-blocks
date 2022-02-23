602860a0cb07458069c5814525afa70d7d466432
Kiki Prottsman <kiki-lee@users.noreply.github.com>
2022-02-22 16:34:49 -0800

    0.0.2

--- assets/js/binary.js
+++ assets/js/binary.js
@@ -1,4 +1,4 @@
-// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.8.20/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.1","target":"arcade","targetVersion":"1.8.20","repo":"kiki-lee/simple-blocks"}
+// meta={"simUrl":"https://trg-arcade.userpxt.io/v1.8.20/---simulator","cdnUrl":"https://pxt.azureedge.net","version":"0.0.2","target":"arcade","targetVersion":"1.8.20","repo":"kiki-lee/simple-blocks"}
 // total=1201795 new=5.27% cached=89.16% other=5.57%
 (function (ectx) {
 'use strict';
@@ -403,7 +403,7 @@ const pxsim_pxtrt = pxsim.pxtrt;
 const pxsim_numops = pxsim.numops;
 
 
-function _main___P73126(s) {
+function _main___P73443(s) {
 let r0 = s.r0, step = s.pc;
 s.pc = -1;
 
@@ -753,13 +753,13 @@ switch (step) {
     return leave(s, r0)
   default: oops()
 } } }
-_main___P73126.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
-_main___P73126.continuations = [  ]
+_main___P73443.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"main.ts","functionName":"<main>","argumentNames":[]}
+_main___P73443.continuations = [  ]
 
-function _main___P73126_mk(s) {
+function _main___P73443_mk(s) {
     checkStack(s.depth);
     return {
-        parent: s, fn: _main___P73126, depth: s.depth + 1,
+        parent: s, fn: _main___P73443, depth: s.depth + 1,
         pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
   tmp_0: undefined,
   tmp_1: undefined,
@@ -40758,5 +40758,5 @@ const sprites_BaseSpriteSayRenderer__C55314_VT = mkVTable({
 
 const breakpoints = setupDebugger(1, [])
 
-return _main___P73126
+return _main___P73443
 })
